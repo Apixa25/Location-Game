3fe516dd04996cd5a500ed7754d24c41abba248d
Steven Sills II <stevensills2@gmail.com>
2026-01-12 18:16:18 -0800

    Index fix

--- index.js
+++ index.js
@@ -2,8 +2,15 @@
  * @format
  */
 
-import {AppRegistry} from 'react-native';
+import {AppRegistry, LogBox} from 'react-native';
 import App from './App';
 import {name as appName} from './app.json';
+
+// Disable yellow box warnings in development for cleaner testing
+LogBox.ignoreLogs([
+  'The result of getSnapshot should be cached', // Zustand selector warning
+  'Non-serializable values were found in the navigation state',
+  'Require cycle:', // Common in RN projects
+]);
 
 AppRegistry.registerComponent(appName, () => App);
